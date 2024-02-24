3437dad5dd0653179d73868c63eccd59803fed02
Dani <danielchenelcea@gmail.com>
2024-02-24 12:50:41 +0100

    Improvements

--- acestream/entrypoint.sh
+++ acestream/entrypoint.sh
@@ -2,9 +2,9 @@
 /etc/init.d/redsocks restart
 iptables -t nat -A OUTPUT -p tcp --dport 80 -j REDIRECT --to-port 12345
 iptables -t nat -A OUTPUT -p tcp --dport 443 -j REDIRECT --to-port 12345
-echo "Waiting for WARP"
+echo "Waiting for WARP...."
 sleep 30
-curl -sf https://cloudflare.com/cdn-cgi/trace
-sleep 30
+curl -s https://cloudflare.com/cdn-cgi/trace
+echo "Starting Acestream...."
 /opt/acestream/start-engine @/opt/acestream/acestream.conf
 
